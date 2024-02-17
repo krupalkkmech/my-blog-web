481fea9dedecb753ef5590ac87681985a32c6721
krupal1710 <krupal@tagmqango.com>
2024-02-17 12:15:23 +0530

    Update folder structure and add design library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import BlogList from "./page/BlogList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<BlogList />}></Route>
     </Routes>
   </BrowserRouter>,
 );
