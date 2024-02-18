b3f14f9d990b2b1052030c3817bc8274b36b9dae
krupal1710 <krupal@tagmqango.com>
2024-02-19 00:54:41 +0530

    Make changes for setup the blog creation form

--- src/page/blogList/BlogForm.jsx
+++ src/page/blogList/BlogForm.jsx
@@ -1,58 +1,64 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Drawer, Space, Form, Input } from 'antd';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import { convert } from 'html-to-text';
+// import htmlToDraft from 'html-to-draftjs';
+import { PlusOutlined } from '@ant-design/icons';
 
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { Button, Drawer, Space, Form, Input, Divider, Select } from 'antd';
+let index = 0;
 function BlogForm({ onClose, onOKClick, open }) {
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+  const inputRef = useRef(null);
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const addItem = (e) => {
+    e.preventDefault();
+    setItems([...items, name || `New item ${index + 1}`]);
+    setName('');
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+  const onFinish = (values) => {
+    console.log(editorState);
+    const html = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+    const text = convert(html, {});
+    console.log(text); // Hello World
+    console.log(html);
+    console.log('Success:', values);
+  };
   return (
-    <Drawer
-      title={`Create Blog`}
-      placement="right"
-      size={'large'}
-      onClose={onClose}
-      open={open}
-      extra={
-        <Space>
-          <Button onClick={onClose}>Cancel</Button>
-          <Button type="primary" onClick={onOKClick}>
-            OK
-          </Button>
-        </Space>
-      }
-    >
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8
-        }}
-        wrapperCol={{
-          span: 16
-        }}
-        style={{
-          maxWidth: 600
-        }}
-        initialValues={{
-          remember: true
-        }}
-        // onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
+    <Drawer title={`Create Blog`} placement="right" width={900} onClose={onClose} open={open}>
+      <Form onFinish={onFinish} autoComplete="off">
+        <p> Title: </p>
         <Form.Item
-          label="Username"
-          name="username"
+          label={null}
+          name="title"
           rules={[
             {
               required: true,
-              message: 'Please input your username!'
+              message: 'Please enter title!'
             }
           ]}
         >
-          <Input />
+          <Input style={{ width: '100%' }} />
         </Form.Item>
 
+        <p> Description: </p>
         <Form.Item
-          label="Password"
-          name="password"
+          label={null}
+          name="description"
           rules={[
             {
               required: true,
@@ -60,7 +66,80 @@ function BlogForm({ onClose, onOKClick, open }) {
             }
           ]}
         >
-          <Input />
+          <Editor
+            editorState={editorState}
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            onEditorStateChange={onEditorStateChange}
+          />
+        </Form.Item>
+        <p> Blog type: </p>
+        <Form.Item
+          label={null}
+          name="type"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter type!'
+            }
+          ]}
+        >
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+        <p> Key words: </p>
+        <Form.Item
+          label={null}
+          name="keyWords"
+          rules={[
+            {
+              required: true,
+              message: 'Please enter Key words!'
+            }
+          ]}
+        >
+          <Select
+            style={{
+              width: '100%'
+            }}
+            mode="multiple"
+            placeholder="custom dropdown render"
+            dropdownRender={(menu) => (
+              <>
+                {menu}
+                <Divider
+                  style={{
+                    margin: '8px 0'
+                  }}
+                />
+                <Space
+                  style={{
+                    padding: '0 8px 4px'
+                  }}
+                >
+                  <Input
+                    placeholder="Please enter item"
+                    ref={inputRef}
+                    value={name}
+                    onChange={onNameChange}
+                    onKeyDown={(e) => e.stopPropagation()}
+                  />
+                  <Button type="text" icon={<PlusOutlined />} onClick={addItem}>
+                    Add item
+                  </Button>
+                </Space>
+              </>
+            )}
+            options={items.map((item) => ({
+              label: item,
+              value: item
+            }))}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </Form.Item>
       </Form>
     </Drawer>
